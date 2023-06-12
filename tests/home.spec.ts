9b4e2edf29abe3cb8b223c379149e979d968fc9a
Thomas De Pauw <thdepauw@hotmail.com>
2023-06-12 21:09:24 +0200

    Home - upcoming group rides Tests

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,4 +2,10 @@ import { test, expect } from "@playwright/test";
 
 test("should go to the home page", async ({ page }) => {
   await page.goto("/");
+  await page.getByRole("button", { name: "close preview warning" }).click();
+
+  await expect(page.getByRole("heading", { name: "Upcoming Group Rides" })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Wachtebeke Explorer Tour" })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Puyenbroek Green Mtb track" })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Blaarmeersen Skatepark" })).toBeVisible();
 });
